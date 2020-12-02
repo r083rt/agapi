c3534da8f3da320420d97cfc39207b15705a3986
unknown <rootrieqyns13@gmail.com>
2020-12-02 16:11:49 +0700

    add laravel custom view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\Assigment;
 use App\Observers\AssigmentSessionObserver;
 use App\Observers\AssigmentObserver;
 use App\Models\Session;
+use Illuminate\Support\Facades\Schema;
 // use App\Models\Like;
 use App\Observers\SessionObserver;
 // use App\Observers\LikeObserver;
@@ -31,9 +32,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         AssigmentSession::observe(AssigmentSessionObserver::class);
         Assigment::observe(AssigmentObserver::class);
         Session::observe(SessionObserver::class);
+        
         // Like::observe(LikeObserver::class);
 
     }
---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,62 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops!')
+@else
+# @lang('Hello!')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('Regards'),<br>{{ config('app.name') }}
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+@slot('subcopy')
+@lang(
+    "Setelah anda sudah klik verifikasi anda bisa login melalui aplikasi AGPAII DIGITAL, Jika anda mengalami kesulitan silahkan klik tombol \":actionText\", copy and paste ke URL dibawah\n".
+    'ke web browser anda: [:actionURL](:actionURL)',
+    [
+        'actionText' => $actionText,
+        'actionURL' => $actionUrl,
+    ]
+)
+@endslot
+@endisset
+@endcomponent
