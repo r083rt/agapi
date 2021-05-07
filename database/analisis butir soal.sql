7efce16b1fbe5bbf4c88e6c7a9ded104b3df39f8
unknown <rootrieqyns13@gmail.com>
2021-05-07 13:12:40 +0700

    update sql

--- database/analisis butir soal.sql
+++ database/analisis butir soal.sql
@@ -1,12 +1,16 @@
 select aql.id,aql.question_list_id,aql.assigment_id,u.name as user_name,g.description as grade,ql.name as question_list_name,ql.created_at,(
-	select group_concat(q.score) from questions q where exists(
-		select 1 from question_lists ql2 where ql2.id=q.question_list_id and ql2.ref_id=ql.id
-    )
+	select group_concat(if(q.score is null,0,q.score)) from questions q 
+		inner join question_lists ql2 on q.question_list_id=ql2.id 
+        inner join assigment_question_lists aql2 on aql2.question_list_id=q.question_list_id
+        inner join assigments a2 on a2.id=aql2.assigment_id
+	where ql2.ref_id=ql.id and a2.teacher_id is not null #teacher_id jika NULL, maka soalnya adalah soal master (latihan mandiri), tidak soal yg dibagikan (kerjakan soal)
 ) as scores,
 (
-	select count(q.score) from questions q where exists(
-		select 1 from question_lists ql2 where ql2.id=q.question_list_id and ql2.ref_id=ql.id
-    )
+	select count(1) from questions q 
+		inner join question_lists ql2 on q.question_list_id=ql2.id 
+        inner join assigment_question_lists aql2 on aql2.question_list_id=q.question_list_id
+        inner join assigments a2 on a2.id=aql2.assigment_id
+	where ql2.ref_id=ql.id and a2.teacher_id is not null #teacher_id jika NULL, maka soalnya adalah soal master (latihan mandiri), tidak soal yg dibagikan (kerjakan soal)
 ) as scores_count
  from assigment_question_lists aql 
 	inner join assigments a on a.id=aql.assigment_id 
@@ -20,4 +24,4 @@ where
 #having scores is not null
     
     
-ORDER BY scores_count desc
+ORDER BY aql.id desc
