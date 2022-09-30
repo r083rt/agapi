7901509a8dfa7b2e98aa5aa1e45c70a543cc5c50
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2022-09-30 13:49:23 +0700

    Update AttendanceController.php

--- app/Http/Controllers/API/v1/AttendanceController.php
+++ app/Http/Controllers/API/v1/AttendanceController.php
@@ -51,7 +51,7 @@ class AttendanceController extends Controller
 
         $event = Event::findOrFail($request->event_id);
         // $event_guest = new EventGuest(['user_id'=>$request->user_id]);
-        $event->users()->sync($request->user_id, false);
+        $event->users()->attach($request->user_id);
         return response()->json($event->load('users'));
     }
 
