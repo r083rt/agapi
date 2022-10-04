72830c0b47956ae6d95ddf80e753414014d41e7c
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-04 10:38:11 +0700

    Update kartu-tanda-anggota.blade.php

--- resources/views/member-card/kartu-tanda-anggota.blade.php
+++ resources/views/member-card/kartu-tanda-anggota.blade.php
@@ -30,15 +30,36 @@
                     border-radius: 50%;
                     ">
         </div>
-        <div style="flex:6; display:flex; justify-content: center; padding-top: 50px">
+        <div style="flex:3; display:flex; justify-content: center; padding-top: 20px">
             <div style="display:flex; flex-direction:column; align-items: center">
                 <h1 style="font-size: 30px; font-weight: bold; ">{{ $user->name }}</h1>
-                <h2 style="font-size: 20px; font-weight: bold; ">{{ $user->role->display_name }}
-                    {{ $user->profile->school_place }}</h2>
-                <h2 style="font-size: 20px; font-weight: bold; ">{{ $user->email }}</h2>
+                <div style="font-size: 20px; font-weight: bold; padding-bottom:10px">({{ $user->role->display_name }})
+                </div>
+                <div style="font-size: 20px; padding-bottom:10px">{{ $user->kta_id }}</div>
+                <div style="font-size: 20px; padding-bottom:10px">{{ $user->profile->school_place }}
+                </div>
+                <div style="font-size: 20px; padding-bottom:10px">{{ $user->email }}</div>
+
             </div>
         </div>
+        <div style="flex:3;display:flex; justify-content:center">
+            <div id="qrcode"></div>
+        </div>
     </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"
+        integrity="sha512-CNgIRecGo7nphbeZ04Sc13ka07paqdeTu0WR1IM4kNcpmBAUSHSQX0FslNhTDadL4O5SAGapGt4FodqL8My0mA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        var qrcode = new QRCode("qrcode", {
+            text: "{{ $user->kta_id }}",
+            width: 200,
+            height: 200,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    </script>
 </body>
 
 </html>
