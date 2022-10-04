e2cd8fbeb12901cacda9795a67b9af7034a957bc
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-04 10:26:04 +0700

    update

--- app/Http/Controllers/UserMemberCardController.php
+++ app/Http/Controllers/UserMemberCardController.php
@@ -15,6 +15,7 @@ class UserMemberCardController extends Controller
     public function index(User $user)
     {
         //
+        $user = $user->load('profile.province', 'profile.city', 'role');
         $storageUrl = env('STORAGE_URL', env('APP_URL', 'localhost:8000'));
         return view('member-card.kartu-tanda-anggota', compact('user', 'storageUrl'));
     }
--- resources/views/member-card/kartu-tanda-anggota.blade.php
+++ resources/views/member-card/kartu-tanda-anggota.blade.php
@@ -9,16 +9,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kartu Tanda Anggota AGPAII</title>
+    <style>
+    </style>
 </head>
 
 <body>
-    <div style="width: 100vw; height: 100vh; background-image: url('/img/membercard.jpeg'); background-size: cover;">
-        <div style="padding: 20px 20px 0 20px;">
-            <div style="display: flex; justify-content: space-between;">
-                <div style="font-size: 20px; font-weight: bold;">{{ $user->name }}</div>
-                <div style="font-size: 20px; font-weight: bold;">{{ $user->id }}</div>
+    <div
+        style="
+            width: 100vw;
+            height: 100vh;
+            background-image: url('/img/membercard.jpeg');
+            background-size: cover;
+            display:flex;
+            flex-direction:column">
+        <div style="display:flex; flex:5; align-items: flex-end; justify-content: center">
+            <img src="{{ $storageUrl }}/{{ $user->avatar }}" alt="photo"
+                style="
+                    width: 150px;
+                    height: 150px;
+                    border-radius: 50%;
+                    ">
+        </div>
+        <div style="flex:6; display:flex; justify-content: center; padding-top: 50px">
+            <div style="display:flex; flex-direction:column; align-items: center">
+                <h1 style="font-size: 30px; font-weight: bold; ">{{ $user->name }}</h1>
+                <h2 style="font-size: 20px; font-weight: bold; ">{{ $user->role->display_name }}
+                    {{ $user->profile->school_place }}</h2>
+                <h2 style="font-size: 20px; font-weight: bold; ">{{ $user->email }}</h2>
             </div>
-            <div style="font-size: 20px; font-weight: bold;">{{ $user->email }}</div>
         </div>
     </div>
 </body>
