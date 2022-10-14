22ddc607913affda37b6e0d93c6a0da37f5b847d
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-14 13:11:10 +0700

    Update Post.php

--- app/Models/Member/Post.php
+++ app/Models/Member/Post.php
@@ -11,23 +11,47 @@ class Post extends Model
 
     protected $guarded = ['id'];
 
+    protected $appends = ['is_liked', 'is_bookmarked', 'likes_count', 'comments_count'];
+
     public function likes()
     {
-        return $this->morphMany('App\Models\Like', 'like');
+        return $this->morphMany('App\Models\Member\Like', 'like');
     }
 
     public function images()
     {
-        return $this->morphMany('App\Models\File', 'file')->whereIn('type', ['image/jpeg', 'image/png', 'image/gif', 'image/jpg']);
+        return $this->morphMany('App\Models\Member\File', 'file')->whereIn('type', ['image/jpeg', 'image/png', 'image/gif', 'image/jpg']);
     }
 
     public function videos()
     {
-        return $this->morphMany('App\Models\File', 'file')->whereIn('type', ['video/mp4', 'video/ogg', 'video/webm']);
+        return $this->morphMany('App\Models\Member\File', 'file')->whereIn('type', ['video/mp4', 'video/ogg', 'video/webm']);
     }
 
     public function author()
     {
-        return $this->belongsTo('App\Models\User', 'author_id', 'id');
+        return $this->belongsTo('App\Models\Member\User', 'author_id', 'id');
+    }
+
+    public function getIsLikedAttribute()
+    {
+        $user = auth('api')->user();
+        return $this->morphOne('App\Models\Member\Like', 'like')->where('user_id', $user ? $user->id : 0)->exists();
+    }
+
+    public function getIsBookmarkedAttribute()
+    {
+        $user = auth('api')->user();
+        return $this->morphOne('App\Models\Member\Bookmark', 'bookmark')->where('user_id', $user ? $user->id : 0)->exists();
+    }
+
+    public function getLikesCountAttribute()
+    {
+        return $this->likes()->count();
+    }
+
+    public function getCommentsCountAttribute()
+    {
+        return $this->comments()->count();
     }
 }
