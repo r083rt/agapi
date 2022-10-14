819e884686701baaf588adf67ea8cdcd26b1c298
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-14 19:38:19 +0700

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -107,9 +107,9 @@ class RouteServiceProvider extends ServiceProvider
         // END ORIGINAL ------------------
 
         // API v1 ROUTE SETTING -------
-        Route::prefix('api')
+        Route::prefix('api/v1/student')
             ->middleware('api')
-            ->namespace($this->namespace)
+            ->namespace(\App\Http\Controllers\API\v1\Student::class)
             ->name('api.v1.student.')
             ->group(base_path('routes/api/v1/student.php'));
         // END ORIGINAL ------------------
