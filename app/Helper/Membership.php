c24118429a052d79647eb9a519cd20ad9161c630
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-18 10:26:38 +0700

    update

---
+++ app/Helper/Membership.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Helper;
+
+use App\Models\User;
+
+class Membership
+{
+
+    // perpanjang user_expired_at berdasarkan user_id
+    public static function renew($user_id, $days)
+    {
+        $user = User::find($user_id);
+        $user->expired_at = now()->addDays($days);
+        $user->save();
+    }
+
+    // tambah masa aktif
+    public static function add($user_id, $days)
+    {
+        $user = User::find($user_id);
+        $user->expired_at = now()->addDays($days);
+        $user->save();
+    }
+
+    // cek apakah user masih aktif
+    public static function isActive($user_id)
+    {
+        $user = User::find($user_id);
+        if ($user->expired_at > now()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ app/Http/Controllers/API/v2/member/MembershipFeeStatusController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\API\v2\member;
+
+use App\Helper\Membership;
+use App\Helper\Midtrans;
+use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class MembershipFeeStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $pendingPayments = Payment::where('user_id', auth()->user()->id)
+            ->where('type', 'Pendaftaran')
+        // today
+            ->whereDate('created_at', now())
+            ->where('status', '!=', 'success')
+            ->get();
+
+        foreach ($pendingPayments as $payment) {
+            $midtrans = new Midtrans();
+            $status = $midtrans->status($payment->midtrans_id);
+            if ($status->transaction_status == 'settlement') {
+                $payment->status = 'success';
+                $payment->save();
+                // tambah masa aktif
+                Membership::add($payment->user_id, 1);
+            }
+        }
+
+        $successPayments = Payment::where('user_id', auth()->user()->id)
+            ->where('type', 'Pendaftaran')
+        // today
+            ->whereDate('created_at', now())
+            ->where('status', 'success')
+            ->count();
+
+        $user = User::find(auth()->user()->id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data pembayaran hari ini',
+            'data' => [
+                'pending' => $pendingPayments,
+                'success' => $successPayments,
+                'user' => $user,
+            ],
+        ]);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
