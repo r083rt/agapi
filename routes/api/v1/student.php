fcf02e72da643a79956a92bade52f2067108bde6
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-20 18:08:34 +0700

    Update student.php

--- routes/api/v1/student.php
+++ routes/api/v1/student.php
@@ -1,31 +1,30 @@
-<?php 
+<?php
 // use App\Http\Controllers\API\v1\Student;
 Route::post('createassigmentsession',
-[App\Http\Controllers\API\v1\Student\AssigmentSessionController::class, 'createAssigmentSession']);
+    [App\Http\Controllers\API\v1\Student\AssigmentSessionController::class, 'createAssigmentSession']);
 
 Route::get('/assigments/search/{key}',
-[App\Http\Controllers\API\v1\Student\AssigmentController::class, 'search']);
+    [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'search']);
 // Route::get('/')
-Route::apiResource('/assigmentsession', Student\AssigmentSessionController::class);
+Route::apiResource('/assigmentsession', App\Http\Controllers\API\v1\Student\AssigmentSessionController::class);
 Route::post('/premium/storeassigmentsession', [App\Http\Controllers\API\v1\Student\AssigmentSessionController::class, 'storePremium']);
 // Route::post('/premium/showassigmentsession', [App\Http\Controllers\API\v1\Student\AssigmentSessionController::class, 'storePremium']);
 
-
 Route::get('unfinishedassigment', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'unfinishedAssigment']);
 
-Route::post('createpayment',[App\Http\Controllers\API\v1\Student\PaymentController::class, 'createPayment']);
+Route::post('createpayment', [App\Http\Controllers\API\v1\Student\PaymentController::class, 'createPayment']);
 // Route::get('balance', [App\Http\Controllers\API\v1\Student\PaymentController::class, 'getBalance']);
 Route::get('checkpayment/{payment_id}', [App\Http\Controllers\API\v1\Student\PaymentController::class, 'checkPayment']);
 Route::get('getpayment/{payment_id}', [App\Http\Controllers\API\v1\Student\PaymentController::class, 'getPayment']);
 
 Route::post('buyassigment', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'buyAssigment']);
 
-Route::get('/ranking', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'ranking'] );
+Route::get('/ranking', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'ranking']);
 
-Route::get('purchasedassignment',[App\Http\Controllers\API\v1\Student\AssigmentController::class, 'purchasedAssignments']);
-Route::get('purchasedassignment/{assignment_id}',[App\Http\Controllers\API\v1\Student\AssigmentController::class, 'showPurchasedAssignment']);
+Route::get('purchasedassignment', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'purchasedAssignments']);
+Route::get('purchasedassignment/{assignment_id}', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'showPurchasedAssignment']);
 
-Route::get('pendingassignmentpayments',[App\Http\Controllers\API\v1\Student\AssigmentController::class, 'pendingAssigmentPayments']);
+Route::get('pendingassignmentpayments', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'pendingAssigmentPayments']);
 
 Route::get('payableassignment', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'payableAssignments']);
 // Route::post('/assigmentsessions/storePremium', [App\Http\Controllers\API\v1\Student\AssigmentController::class, 'storePremium']);
