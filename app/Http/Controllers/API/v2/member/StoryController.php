db1be128ac3d758423d3a292b1687ec3f3bab797
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-20 13:49:44 +0700

    Update StoryController.php

--- app/Http/Controllers/API/v2/member/StoryController.php
+++ app/Http/Controllers/API/v2/member/StoryController.php
@@ -8,6 +8,7 @@ use App\Models\Member\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Thumbnail;
 
 class StoryController extends Controller
 {
@@ -49,16 +50,24 @@ class StoryController extends Controller
             'file' => 'required|file|mimes:jpg,jpeg,png,mp4,mov,ogg,qt',
         ]);
 
-        $file = new File();
-        $file->key = 'story';
-        $file->file_type = 'App\Models\Member\User';
-        $file->file_id = $request->user()->id;
-
-        $extension = $request->file('file')->extension();
-        $fileName = time() . '.' . $extension;
-
+        // return response()->json([
+        //     'message' => 'ok',
+        //     'type' => $request->file('file')->getMimeType(),
+        //     'type2' => $request->file('file')->getClientMimeType(),
+        //     'type3' => $request->file('file')->extension(),
+        //     'type4' => $request->file('file')->getType(),
+        // ]);
+        $mimeType = $request->file('file')->getClientMimeType();
         // jika gambar
-        if ($request->file('file')->isImage()) {
+        if (strpos($mimeType, 'image') !== false) {
+            $file = new File();
+            $file->key = 'story';
+            $file->file_type = 'App\Models\Member\User';
+            $file->file_id = $request->user()->id;
+
+            $extension = $request->file('file')->extension();
+            $fileName = time() . '.' . $extension;
+
             $file->type = 'image/' . $extension;
             $path = 'stories/' . $fileName;
 
@@ -66,17 +75,58 @@ class StoryController extends Controller
                 $constraint->aspectRatio();
             })->encode('jpg', 60);
             Storage::disk(env('FILESYSTEM_DRIVER'))->put($path, $compressed);
+            $file->src = $path;
+            $file->save();
 
         }
 
         // jika video
-        if ($request->file('file')->isVideo()) {
+        if (strpos($mimeType, 'video') !== false) {
+            $file = new File();
+            $file->key = 'story';
+            $file->file_type = 'App\Models\Member\User';
+            $file->file_id = $request->user()->id;
+
+            $extension = $request->file('file')->extension();
+            $fileName = time() . '.' . $extension;
+
             $file->type = 'video/' . $extension;
             $path = $request->file('file')->store('stories', env('FILESYSTEM_DRIVER', 'public'));
-        }
+            $file->src = $path;
+            $file->save();
 
-        $file->src = $path;
-        $file->save();
+            // file type is video
+            // set storage path to store the file (image generated for a given video)
+            $thumbnail_path = public_path() . '/storage/thumbnails';
+//check if folder is exists
+            if (!Storage::disk('public')->exists('thumbnails')) {
+                Storage::disk('public')->makeDirectory('thumbnails', 0777, true); //creates directory
+            }
+
+            // set thumbnail image name
+            $timestamp = time();
+            $thumbnail_image = $request->user()->id . "." . $timestamp . ".jpg";
+// get video length and process it
+            // assign the value to time_to_image (which will get screenshot of video at that specified seconds)
+            // $time_to_image    = floor(($data['video_length'])/2);
+            $time_to_image = 0.1;
+            $thumbnail_status = Thumbnail::getThumbnail($request->file('file'), $thumbnail_path, $thumbnail_image, $time_to_image);
+            $storagePublic = Storage::disk('wasabi')->put('thumbnails/' . $thumbnail_image, Storage::disk('public')->get('thumbnails/' . $thumbnail_image));
+// dd($storagePublic);
+            if ($storagePublic) {
+                Storage::disk('public')->delete('thumbnails/' . $thumbnail_image);
+            }
+
+            $thumbnail = new File();
+
+            $thumbnail->src = $path;
+            $thumbnail->type = $request->file('file')->getClientMimeType();
+            $thumbnail->file_id = $file->id;
+            $thumbnail->file_type = 'App\Models\File';
+            $thumbnail->key = 'thumbnail';
+            $thumbnail->save();
+
+        }
 
         return response()->json($file);
 
