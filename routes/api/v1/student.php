60d06bf7e6736ea51ac388fb3cfda94b1854c8e1
Zulham Azwar Achmad <36602853+zulham724@users.noreply.github.com>
2022-10-20 19:30:59 +0700

    Update student.php

--- routes/api/v1/student.php
+++ routes/api/v1/student.php
@@ -1,37 +1,37 @@
 <?php
 
-Route::post('createassigmentsession', [AssigmentSessionController::class, 'createAssigmentSession']);
+Route::post('createassigmentsession', 'AssigmentSessionController@createAssigmentSession');
 
-Route::get('/assigments/search/{key}', [AssigmentController::class, 'search']);
+Route::get('/assigments/search/{key}', 'AssigmentController@search');
 // Route::get('/')
-Route::apiResource('/assigmentsession', AssigmentSessionController::class);
-Route::post('/premium/storeassigmentsession', [AssigmentSessionController::class, 'storePremium']);
+Route::apiResource('/assigmentsession', 'AssigmentSessionController');
+Route::post('/premium/storeassigmentsession', 'AssigmentSessionController@storePremium');
 // Route::post('/premium/showassigmentsession', [AssigmentSessionController::class, 'storePremium']);
 
-Route::get('unfinishedassigment', [AssigmentController::class, 'unfinishedAssigment']);
+Route::get('unfinishedassigment', 'AssigmentController@unfinishedAssigment');
 
-Route::post('createpayment', [PaymentController::class, 'createPayment']);
+Route::post('createpayment', 'PaymentController@createPayment');
 // Route::get('balance', [PaymentController::class, 'getBalance']);
-Route::get('checkpayment/{payment_id}', [PaymentController::class, 'checkPayment']);
-Route::get('getpayment/{payment_id}', [PaymentController::class, 'getPayment']);
+Route::get('checkpayment/{payment_id}', 'PaymentController@checkPayment');
+Route::get('getpayment/{payment_id}', 'PaymentController@getPayment');
 
-Route::post('buyassigment', [AssigmentController::class, 'buyAssigment']);
+Route::post('buyassigment', 'AssigmentController@buyAssigment');
 
-Route::get('/ranking', [AssigmentController::class, 'ranking']);
+Route::get('/ranking', 'AssigmentController@ranking');
 
-Route::get('purchasedassignment', [AssigmentController::class, 'purchasedAssignments']);
-Route::get('purchasedassignment/{assignment_id}', [AssigmentController::class, 'showPurchasedAssignment']);
+Route::get('purchasedassignment', 'AssigmentController@purchasedAssignments');
+Route::get('purchasedassignment/{assignment_id}', 'AssigmentController@showPurchasedAssignment');
 
-Route::get('pendingassignmentpayments', [AssigmentController::class, 'pendingAssigmentPayments']);
+Route::get('pendingassignmentpayments', 'AssigmentController@pendingAssigmentPayments');
 
-Route::get('payableassignment', [AssigmentController::class, 'payableAssignments']);
+Route::get('payableassignment', 'AssigmentController@payableAssignments');
 // Route::post('/assigmentsessions/storePremium', [AssigmentController::class, 'storePremium']);
 
-Route::get('paidassignmentdetails/{assigment_id}', [AssigmentController::class, 'paidAssignmentDetails']);
+Route::get('paidassignmentdetails/{assigment_id}', 'AssigmentController@paidAssignmentDetails');
 
-Route::post('placeassignmentpayment/{assigment_id}', [AssigmentController::class, 'placeAssignmentPayment']);
-Route::get('getassignmentpaymentdetails/{assigment_id}', [AssigmentController::class, 'getPayment']);
+Route::post('placeassignmentpayment/{assigment_id}', 'AssigmentController@placeAssignmentPayment');
+Route::get('getassignmentpaymentdetails/{assigment_id}', 'AssigmentController@getPayment');
 
-Route::put('confirmpayment/{payment_id}', [PaymentController::class, 'confirmPayment']);
+Route::put('confirmpayment/{payment_id}', 'PaymentController@confirmPayment');
 
-Route::get('checkassignmentpayment/{assignment_id}', [AssigmentController::class, 'checkAssignmentPayment']);
+Route::get('checkassignmentpayment/{assignment_id}', 'AssigmentController@checkAssignmentPayment');
