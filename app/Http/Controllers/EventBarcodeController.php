d18ccb4e2dd9bc903d7438d5e69fea26aafca733
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2022-11-03 12:15:06 +0700

    Update EventBarcodeController.php

--- app/Http/Controllers/EventBarcodeController.php
+++ app/Http/Controllers/EventBarcodeController.php
@@ -24,12 +24,16 @@ class EventBarcodeController extends Controller
         // return "$url/user/$userId/member-card";
         $headers = array('Content-Type: image/png');
         // header('Content-Disposition: attachment; filename="' . $event->name . '.jpg"');
+
+        //convert to image
         $file = Browsershot::url("$url/event/$eventId/barcode")
-            // ->noSandbox()
-            // ->windowSize(600, 600)
-            // ->fullPage()
-            // ->setNodeBinary(env('NODE_BINARY_PATH', '/usr/bin/node'))
-            // ->setNpmBinary(env('NPM_BINARY_PATH', '/usr/bin/npm'))
-            ->save('barcode.jpg');
+            ->noSandbox()
+            ->windowSize(600, 600)
+            ->fullPage()
+            ->setNodeBinary(env('NODE_BINARY_PATH', '/usr/bin/node'))
+            ->setNpmBinary(env('NPM_BINARY_PATH', '/usr/bin/npm'))
+            ->save('barcode.png');
+
+        return response()->download($file, $event->name . '.png', $headers);
     }
 }
