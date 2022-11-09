037eac9c6030cba98c205a968d9d59f6ac5c14a6
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2022-11-09 11:18:54 +0700

    Update member.php

--- routes/api/v2/member.php
+++ routes/api/v2/member.php
@@ -60,7 +60,7 @@ Route::group(['middleware' => 'auth:api'], function () {
         'setting' => 'SettingController', //untuk mendapatkan data dari table setting
     ]);
 
-    Route::get('/user/total-member', 'UserController@gettotalmember');
+    Route::get('/users/total-member', 'UserController@gettotalmember');
 
     Route::get('/personal-conversation/search/{keyword}', 'PersonalConversationController@search');
 
