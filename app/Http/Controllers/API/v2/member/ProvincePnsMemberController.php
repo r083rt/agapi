045af814812ed7f10f4156a438a18576a646505c
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2022-11-09 10:41:44 +0700

    update

---
+++ app/Http/Controllers/API/v2/member/ProvincePnsMemberController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API\v2\member;
+
+use App\Http\Controllers\Controller;
+use App\Models\Province;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProvincePnsMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($provinceId)
+    {
+        //
+        $province = Province::withCount(['users' => function($query){
+            $query->whereHas('pns_status', function($query){
+                $query->where('is_pns', '1');
+            });
+        }])->find($provinceId);
+
+        return response()->json($province);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
