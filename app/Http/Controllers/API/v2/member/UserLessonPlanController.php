a155db8b04b4f68daea488547681034fdd741aac
Rian Eko <rian.eko1213@gmail.com>
2022-12-14 13:38:40 +0700

    update

--- app/Http/Controllers/API/v2/member/UserLessonPlanController.php
+++ app/Http/Controllers/API/v2/member/UserLessonPlanController.php
@@ -101,6 +101,41 @@ class UserLessonPlanController extends Controller
     public function update(Request $request, $id)
     {
         //
+        //convert $request->cover to file
+        $cover = $request->cover;
+        $cover = str_replace('data:image/png;base64,', '', $cover);
+        $cover = str_replace(' ', '+', $cover);
+        $cover = base64_decode($cover);
+
+
+        $lessonplan = LessonPlan::findOrFail($request['id']);
+        $lessonplan->topic = json_decode($request->topic);
+        $lessonplan->description = json_decode($request->description);
+        $lessonplan->grade_id = $request->grade_id;
+        $lessonplan->subject = json_decode($request->subject);
+        $lessonplan->duration = json_decode($request->duration);
+        $lessonplan->creator_id = $request->user()->id;
+        $lessonplan->school = $request->user()->profile->school_place ?? 'Kosong';
+        $lessonplan->effort = 100;
+
+        //upload cover
+        $fileName = time() . '.' . 'png';
+        $compressedImage = \Image::make($cover)->resize(1080, null, function ($constraint) {
+            $constraint->aspectRatio();
+        })->encode('jpg', 60);
+        $folderPath = "cover";
+        $path = "{$folderPath}/{$fileName}";
+        Storage::disk(env('FILESYSTEM_DRIVER', 'wasabi'))->put($path, $compressedImage);
+        $lessonplan->image = $path;
+
+        $lessonplan->save();
+
+        if ($request->has('contents')) {
+            $lessonplan->contents()->delete();
+            $lessonplan->contents()->createMany($request->contents);
+        }
+
+        return response()->json($lessonplan);
     }
 
     /**
