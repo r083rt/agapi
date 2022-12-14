054c453fc0c82c51b8f73261604d6bbbe47ad142
Rian Eko <rian.eko1213@gmail.com>
2022-12-14 11:15:37 +0700

    update

--- app/Http/Controllers/API/v2/member/UserLessonPlanController.php
+++ app/Http/Controllers/API/v2/member/UserLessonPlanController.php
@@ -81,9 +81,14 @@ class UserLessonPlanController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($userId, $lessonPlanId)
     {
         //
+        $lessonplans = LessonPlan::with([
+            'grade',
+        ])->findOrFail($lessonPlanId);
+
+        return response()->json($lessonplans);
     }
 
     /**
