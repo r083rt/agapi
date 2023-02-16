6a0519cf633e6ef924dae68d08bc2d2d2ec8f361
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2023-02-16 11:39:23 +0700

    Update UserController.php

--- app/Http/Controllers/API/v2/admin/UserController.php
+++ app/Http/Controllers/API/v2/admin/UserController.php
@@ -36,22 +36,27 @@ class UserController extends Controller
         return response()->json($users);
     }
 
-    public function memberGrowth()
-    {
+    public function memberGrowth(){
         // ambil count users dari awal sampai 2 bulan lalu
-        $usersTill2MonthAgo = Payment::where('value', 35000)
-            ->where('status', 'success')
+        $usersTill2MonthAgo = User::where('user_activated_at', '!=', null)
+            ->whereHas('role', function ($query) {
+                $query->whereIn('name', ['user']);
+            })
             ->whereDate('created_at', '<', date('Y-m-d', strtotime('-2 month')))
             ->count();
 
         // ambil count users dari awal sampai bulan lalu
-        $usersTillLastMonth = Payment::where('value', 35000)
-            ->where('status', 'success')
+        $usersTillLastMonth = User::where('user_activated_at', '!=', null)
+            ->whereHas('role', function ($query) {
+                $query->whereIn('name', ['user']);
+            })
             ->whereDate('created_at', '<', date('Y-m-d', strtotime('-1 month')))
             ->count();
         // ambil count users dari awal sampai bulan ini
-        $usersNow = Payment::where('value', 35000)
-            ->where('status', 'success')
+        $usersNow = User::where('user_activated_at', '!=', null)
+            ->whereHas('role', function ($query) {
+                $query->whereIn('name', ['user']);
+            })
             ->whereDate('created_at', '<', date('Y-m-d'))
             ->count();
 
