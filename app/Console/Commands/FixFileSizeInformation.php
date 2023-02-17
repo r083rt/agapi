5ae7137b766a80b093a6729fb85ea80d94d1b648
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2023-02-17 11:24:05 +0700

    Create FixFileSizeInformation.php

---
+++ app/Console/Commands/FixFileSizeInformation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\File;
+// Storage
+use Illuminate\Support\Facades\Storage;
+
+class FixFileSizeInformation extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fix:file-size-information';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // get all files where size is null
+        $files = File::whereNull('size')->get();
+
+        // loop through each file
+        foreach ($files as $file) {
+            // get the file size
+            // jika file->src adalah struktur json maka skip
+            if (json_decode($file->src)) {
+                $this->info("{$file->src} => skipped");
+                continue;
+            }
+            // $size = filesize("https://cdn-agpaiidigital.online/{$file->src}");
+            // get size from storage
+            $size = Storage::disk('wasabi')->size($file->src);
+            // $this->info("{$file->src} => {$size}");
+            // update the file size
+            $file->update([
+                'size' => $size
+            ]);
+            $this->info("{$file->src} => {$size} => updated");
+        }
+        return 0;
+    }
+}
