a9d3f6e094fbec3b5655c83eb76bf38b82f7725c
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2023-02-17 12:53:47 +0700

    Create SyncRegisterPayment.php

---
+++ app/Console/Commands/SyncRegisterPayment.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Helper\Midtrans;
+
+class SyncRegisterPayment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'payment:sync-register-payment';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = User::where('user_activated_at', null)
+            ->whereHas('payments', function ($query) {
+                return $query->where('value', 35000);
+            })
+            ->with(['payments' => function ($query) {
+                return $query->where('value', 35000);
+            }])->get();
+
+        $usersCount = $users->count();
+
+        foreach ($users as $u => $user) {
+            $percentage = round(($u + 1) / $usersCount * 100, 2);
+
+            $paymentsCount = $user->payments->count();
+
+            foreach ($user->payments as $p => $payment) {
+                $midtrans = new Midtrans();
+                try {
+                    $status = $midtrans->status($payment->midtrans_id);
+
+                    $this->info("{$p}/{$paymentsCount} => Pembayaran berstatus {$status->transaction_status}");
+
+                    if ($status->transaction_status == 'settlement') {
+                        $result = $payment->setSuccess();
+
+                        $this->info("{$percentage}% ({$u}/{$usersCount}) {$user->email} => Pembayaran telah di konfirmasi");
+                    }
+                } catch (\Exception $e) {
+                    $this->info("{$percentage}% {$user->email} => {$e->getMessage()}");
+                }
+            }
+        }
+
+        return 0;
+    }
+}
